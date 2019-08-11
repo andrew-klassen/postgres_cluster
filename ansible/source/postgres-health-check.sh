00e58e85b1eb79769d1257b9e1ee951bd1582699
Andrew Klassen <aklassen80@yahoo.com>
2019-08-11 21:33:05 +0000

    working

--- ansible/source/postgres-health-check.sh
+++ ansible/source/postgres-health-check.sh
@@ -7,9 +7,11 @@ do
 	has_cluster_ip=$(ip a | grep "{{ cluster_ip }}" | wc -l)
 
 	if [[ ${has_cluster_ip} > 0 ]]; then
-		systemctl stop postgresql
-	else
 		systemctl start postgresql
+	else
+		systemctl stop postgresql
 	fi
+
+	sleep .1
 
 done
